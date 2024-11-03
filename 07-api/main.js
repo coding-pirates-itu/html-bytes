e91369da8c952bc251ebaf13cb5ac76481c95d0a
Mike <mnikitin0@gmail.com>
2024-11-03 12:37:57 +0100

    Added house, lists, forms, API

---
+++ 07-api/main.js
@@ -0,0 +1,34 @@
+async function process(elemId) {
+    // https://ip-api.com/docs/api:json
+    let locationUrl = 'http://ip-api.com/json/?fields=status,message,country,countryCode,regionName,city,lat,lon'
+
+    async function getJson(url) {
+        let response = await fetch(url);
+        let data = await response.json();
+        return data;
+    }
+    
+    // https://open-meteo.com/
+    function getWeatherUrl(lat, lon) {
+        return `https://api.open-meteo.com/v1/forecast?latitude=${lat}&longitude=${lon}&current=temperature_2m,wind_speed_10m,wind_direction_10m`
+    }
+
+    function formatLocation(d) {
+        return `${d.country} (${d.regionName}), ${d.city}`;
+    }
+
+    function formatWeather(d) {
+        return `Temp ${d.current.temperature_2m} ${d.current_units.temperature_2m}, wind ${d.current.wind_speed_10m} ${d.current_units.wind_speed_10m}`;
+    }
+
+    let elem = document.getElementById(elemId);
+    elem.innerHTML += '<hr/>' + new Date().toLocaleString();
+
+    getJson(locationUrl)
+        .then(loc => {
+            elem.innerHTML += '<br/>' + formatLocation(loc)
+            return getJson(getWeatherUrl(loc.lat, loc.lon))})
+        .then(weather => elem.innerHTML += '<br/>' + formatWeather(weather));
+
+    return false;
+}
--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Coding Pirates education material: HTML bytes, small HTML tasks.
 1. [Text tags](01-text/index.html)
 2. [Images, favicon](02-images/index.html)
 3. [Styles](03-styles/index.html)
-4. [Tables](04-tables/index.html)
-5. Forms
+4. [Tables](04-tables/table.html) and [lists](04-tables/lists.html)
+5. Forms: [br layout](05-forms/form-with-br.html), [table layout](05-forms/form-in-table.html), [calculated](05-forms/form-math.html)
 6. JS basics
-7. JS API
+7. [JS API](07-api/index.html)
